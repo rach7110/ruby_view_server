f345110628732ea8ccc70f10faad9680eed6b507
MakerSquare Student <makersquarestudent@unknown98fe944b77c8.att.net>
2013-06-25 15:28:48 -0500

    step 3 in 'learn.com'

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,11 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = x*5
+template = ERB.new "The value of y is: <%= y%>"
+puts template.result(binding)
+
+animals = %w{cats,  dogs, monkeys, lemurs, frogs}
+template = ERB.new "These are animals: <%= animals%>"
+puts template.result(binding)
